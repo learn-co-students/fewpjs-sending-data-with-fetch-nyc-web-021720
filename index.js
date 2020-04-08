b5b3642c51a6fefe9a3328c29c45d047b8940f50
anoorzi <anoorzi123@gmail.com>
2020-04-08 16:39:32 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your code here
+
+document.addEventListener('DOMContentLoaded', function(event){
+    submitData()
+})
+
+function submitData(){
+    let person = {
+        Name: "Johnson",
+        Email:"bigboy@gmail.com"
+    }
+    let reqObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(person)
+    }
+    
+    
+    fetch('http://localhost:3000/users', reqObj)
+    .then(resp => resp.json())
+    .then(function(data){
+        console.log(data)
+    })
+}
